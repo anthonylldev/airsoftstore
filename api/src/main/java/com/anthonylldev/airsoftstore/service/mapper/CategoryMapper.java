3733bd52b58769324294d552d91b3623ab89c68e
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-09-29 17:57:45 +0200

    Solve category mapper

--- api/src/main/java/com/anthonylldev/airsoftstore/service/mapper/CategoryMapper.java
+++ api/src/main/java/com/anthonylldev/airsoftstore/service/mapper/CategoryMapper.java
@@ -8,4 +8,17 @@ import org.mapstruct.*;
  * Mapper for the entity {@link Category} and its DTO {@link CategoryDTO}.
  */
 @Mapper(componentModel = "spring")
-public interface CategoryMapper extends EntityMapper<CategoryDTO, Category> {}
+public interface CategoryMapper extends EntityMapper<CategoryDTO, Category> {
+
+    @Override
+    @Mapping(target = "subcategories", ignore = true)
+    @Mapping(target = "removeSubcategory", ignore = true)
+    Category toEntity(CategoryDTO dto);
+
+    @Override
+    @Named("partialUpdate")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "subcategories", ignore = true)
+    @Mapping(target = "removeSubcategory", ignore = true)
+    void partialUpdate(@MappingTarget Category entity, CategoryDTO dto);
+}

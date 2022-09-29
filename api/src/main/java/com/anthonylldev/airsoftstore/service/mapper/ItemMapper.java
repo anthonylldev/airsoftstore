d48615ba3ee7f456560b530f48bf99a59465e158
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-09-29 17:59:53 +0200

    Solve item mapper

--- api/src/main/java/com/anthonylldev/airsoftstore/service/mapper/ItemMapper.java
+++ api/src/main/java/com/anthonylldev/airsoftstore/service/mapper/ItemMapper.java
@@ -17,15 +17,39 @@ public interface ItemMapper extends EntityMapper<ItemDTO, Item> {
     @Mapping(target = "subCategory", source = "subCategory", qualifiedByName = "subCategoryTitle")
     ItemDTO toDto(Item s);
 
+    @Override
+    @Mapping(target = "brand", source = "brand", qualifiedByName = "brandEntityTitle")
+    @Mapping(target = "subCategory", source = "subCategory", qualifiedByName = "subCategoryEntityTitle")
+    Item toEntity(ItemDTO dto);
+
+    @Override
+    @Named("partialUpdate")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "brand", source = "brand", qualifiedByName = "brandEntityTitle")
+    @Mapping(target = "subCategory", source = "subCategory", qualifiedByName = "subCategoryEntityTitle")
+    void partialUpdate(@MappingTarget Item entity, ItemDTO dto);
+
     @Named("brandTitle")
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     @Mapping(target = "title", source = "title")
     BrandDTO toDtoBrandTitle(Brand brand);
 
+    @Named("brandEntityTitle")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "title", source = "title")
+    Brand toDtoBrandTitle(BrandDTO brandDTO);
+
     @Named("subCategoryTitle")
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     @Mapping(target = "title", source = "title")
     SubCategoryDTO toDtoSubCategoryTitle(SubCategory subCategory);
+
+    @Named("subCategoryEntityTitle")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "title", source = "title")
+    SubCategory toDtoSubCategoryTitle(SubCategoryDTO subCategoryDTO);
 }

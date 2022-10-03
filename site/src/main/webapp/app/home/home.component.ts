219ebadb910f4bff978a9a69c1189e20e9d1c581
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-03 17:11:19 +0200

    Implement card and update home to show most popular categories

--- site/src/main/webapp/app/home/home.component.ts
+++ site/src/main/webapp/app/home/home.component.ts
@@ -5,6 +5,9 @@ import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { SubCategoryService } from 'app/entities/sub-category/service/sub-category.service';
+import { EntityArrayResponseType } from 'app/entities/category/service/category.service';
+import { ISubCategory } from 'app/entities/sub-category/sub-category.model';
 
 @Component({
   selector: 'jhi-home',
@@ -13,16 +16,22 @@ import { Account } from 'app/core/auth/account.model';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
+  mostPopularSubCategories?: ISubCategory[];
 
   private readonly destroy$ = new Subject<void>();
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    private subCategoryService: SubCategoryService,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
+
+    this.loadMostPopularSubCategories();
   }
 
   login(): void {
@@ -33,4 +42,29 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.destroy$.next();
     this.destroy$.complete();
   }
+
+  private loadMostPopularSubCategories(): void {
+    const queryMostPopularSubCategoriesObject: any = {
+      page: 0,
+      size: 9,
+      eargeload: true,
+      sort: ["accessCount,desc"]
+    }
+
+    this.subCategoryService.query(queryMostPopularSubCategoriesObject).subscribe({
+      next: (res: EntityArrayResponseType) => {
+        this.onResponseSuccess(res);
+      },
+    });
+  }
+
+  private onResponseSuccess(response: EntityArrayResponseType): void {
+    const dataFromBody = this.fillComponentAttributesFromResponseBody(response.body);
+    this.mostPopularSubCategories = dataFromBody;
+  }
+
+  private fillComponentAttributesFromResponseBody(data: ISubCategory[] | null): ISubCategory[] {
+    return data ?? [];
+  }
+
 }

ba4106ef05ac52de13041e6002b0aaab19c22e46
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-06 12:03:46 +0200

    Update item-card style

--- site/src/main/webapp/app/shared/card-item/item-card.component.ts
+++ site/src/main/webapp/app/shared/card-item/item-card.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { faBan, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { IItem } from '../../entities/item/item.model';
 
 @Component({
   selector: 'jhi-item-card',
   templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.scss']
+  styleUrls: ['./item-card.component.scss'],
 })
 export class ItemCardComponent {
   @Input() item?: IItem;
   faBan = faBan;
   faCartShopping = faCartShopping;
 
+  constructor(private router: Router) {}
+
+  navToItem(): void {
+    this.router.navigate(['/item',this.item?.id, 'view']);
+  }
 }

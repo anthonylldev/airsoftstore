2799229ef337069163b9d8d6ad6bab43df6d82a9
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-11 16:33:19 +0200

    Load categories in filter

--- site/src/main/webapp/app/shared/filter/filter.component.ts
+++ site/src/main/webapp/app/shared/filter/filter.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ICategory } from 'app/entities/category/category.model';
+import { CategoryService } from 'app/entities/category/service/category.service';
+import { EntityArrayResponseType } from 'app/entities/sub-category/service/sub-category.service';
 import { IFilterOptions } from './filter.model';
 
 @Component({
   selector: 'jhi-filter',
   templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
   @Input() filters!: IFilterOptions;
+  categories?: ICategory[];
+
+  constructor(
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
 
   clearAllFilters(): void {
     this.filters.clear();
@@ -14,5 +27,26 @@ export class FilterComponent {
 
   clearFilter(filterName: string, value: string): void {
     this.filters.removeFilter(filterName, value);
+  }
+
+  private loadCategories(): void {
+    const queryCategoriesObject: any = {
+      sort: ['title,asc'],
+    };
+
+    this.categoryService.query(queryCategoriesObject).subscribe({
+      next: (res: EntityArrayResponseType) => {
+        this.onCategoryResponseSuccess(res);
+      },
+    });
+  }
+
+  private onCategoryResponseSuccess(response: EntityArrayResponseType): void {
+    const dataFromBody = this.fillCategoryFromResponseBody(response.body);
+    this.categories = dataFromBody;
+  }
+
+  private fillCategoryFromResponseBody(data: ICategory[] | null): ICategory[] {
+    return data ?? [];
   }
 }

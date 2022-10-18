2af424b685f1bfecb3955b49c5decdb086496ad9
Anthony Leon <anthonyfabian22ll@gmail.com>
2022-10-18 16:27:19 +0200

    Style sub categories

--- site/src/main/webapp/app/entities/sub-category/list/sub-category.component.ts
+++ site/src/main/webapp/app/entities/sub-category/list/sub-category.component.ts
@@ -16,6 +16,7 @@ import { FilterOptions, IFilterOptions, IFilterOption } from 'app/shared/filter/
 @Component({
   selector: 'jhi-sub-category',
   templateUrl: './sub-category.component.html',
+  styleUrls: ["./sub-category.component.scss"]
 })
 export class SubCategoryComponent implements OnInit {
   subCategories?: ISubCategory[];
